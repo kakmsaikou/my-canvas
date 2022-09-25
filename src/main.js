7f8bff2332511784ea03b8b6bcdac9730b3bec7e
TacitusPie <kakmsaikou@gmail.com>
2022-09-25 12:57:57 +0800

    用对象封装画板

--- src/main.js
+++ src/main.js
@@ -1,81 +1,91 @@
-// 获取canvas对象
-const canvas = document.getElementById('canvas')
+const myCanvas = {
+  ui: {
+    // 获取canvas对象
+    canvas: document.querySelector('#canvas')
+  },
 
-// 获取用户文档（document）的宽高，设置canvas的宽高
-canvas.width = document.documentElement.clientWidth
-canvas.height = document.documentElement.clientHeight
+  ctx: undefined,
 
-// 创建drawing context
-let ctx = canvas.getContext('2d')
+  // 设置信号，来判断键鼠下鼠标的点击和弹起状态
+  painting: false,
 
-// 填充颜色
-ctx.fillStyle = 'black'
+  // 设置上一次的坐标
+  last: undefined,
 
-// 设置画笔粗细
-ctx.lineWidth = 10
+  init: () => {
+    // 创建drawing context
+    myCanvas.ctx = myCanvas.ui.canvas.getContext('2d')
 
-//把画笔末端设为圆形
-ctx.lineCap = 'round'
+    // 获取用户文档（document）的宽高，设置canvas的宽高
+    myCanvas.ui.canvas.width = document.documentElement.clientWidth
+    myCanvas.ui.canvas.height = document.documentElement.clientHeight
 
-// 设置信号，来判断键鼠下鼠标的点击和弹起状态
-let painting = false
+    // 填充颜色
+    myCanvas.ctx.fillStyle = 'black'
 
-// 设置上一次的坐标
-let last
+    // 设置画笔粗细
+    myCanvas.ctx.lineWidth = 10
 
-// 封装画线函数
-function drawLine(x1, y1, x2, y2) {
-  ctx.beginPath()
-  ctx.moveTo(x1, y1)
-  ctx.lineTo(x2, y2)
-  ctx.stroke()
-}
+    //把画笔末端设为圆形
+    myCanvas.ctx.lineCap = 'round'
 
-// 检测浏览器是否支持鼠标
-if (!matchMedia('(pointer:fine)').matches) {
-  // 触屏点击事件
-  canvas.ontouchstart = (e) => {
-    let x = e.touches[0].clientX
-    let y = e.touches[0].clientY
-    last = [x, y]
-  }
+    // 检测浏览器是否支持鼠标
+    // 绑定画线事件
+    if (!matchMedia('(pointer:fine)').matches) {
+      // 触屏点击事件
+      myCanvas.ui.canvas.ontouchstart = (e) => {
+        let x = e.touches[0].clientX
+        let y = e.touches[0].clientY
+        myCanvas.last = [x, y]
+      }
 
-  // 触屏移动事件
-  canvas.ontouchmove = (e) => {
-    let x = e.touches[0].clientX
-    let y = e.touches[0].clientY
-    drawLine(last[0], last[1], x, y)
-    last = [x, y]
-  }
-} else {
-  canvas.onmousedown = (e) => {
-    painting = true
-    // 鼠标按下时的位置设为last的初始值
-    last = [e.clientX, e.clientY]
-  }
+      // 触屏移动事件
+      myCanvas.ui.canvas.ontouchmove = (e) => {
+        let x = e.touches[0].clientX
+        let y = e.touches[0].clientY
+        myCanvas.drawLine(myCanvas.last[0], myCanvas.last[1], x, y)
+        myCanvas.last = [x, y]
+      }
+    } else {
+      myCanvas.ui.canvas.onmousedown = (e) => {
+        myCanvas.painting = true
+        // 鼠标按下时的位置设为last的初始值
+        myCanvas.last = [e.clientX, e.clientY]
+      }
 
-  canvas.onmouseup = () => {
-    painting = false
-  }
+      myCanvas.ui.canvas.onmouseup = () => {
+        myCanvas.painting = false
+      }
 
-  canvas.onmousemove = (e) => {
-    if (painting) {
-      // 画直线
-      drawLine(last[0], last[1], e.clientX, e.clientY)
-      last = [e.clientX, e.clientY]
+      myCanvas.ui.canvas.onmousemove = (e) => {
+        if (myCanvas.painting) {
+          // 画直线
+          myCanvas.drawLine(myCanvas.last[0], myCanvas.last[1], e.clientX, e.clientY)
+          myCanvas.last = [e.clientX, e.clientY]
+        }
+      }
     }
-  }
+  },
+
+  drawLine: (x1, y1, x2, y2)=>{
+    myCanvas.ctx.beginPath()
+    myCanvas.ctx.moveTo(x1, y1)
+    myCanvas.ctx.lineTo(x2, y2)
+    myCanvas.ctx.stroke()
+  },
 }
 
-const bigPen = document.querySelector('#big-pen')
-const normalPen = document.querySelector('#normal-pen')
-const smallPen = document.querySelector('#small-pen')
-bigPen.onclick = () => {
-  ctx.lineWidth = 20
-}
-normalPen.onclick = () => {
-  ctx.lineWidth = 10
-}
-smallPen.onclick = ()=>{
-  ctx.lineWidth = 5
-}
+myCanvas.init()
+
+// const bigPen = document.querySelector('#big-pen')
+// const normalPen = document.querySelector('#normal-pen')
+// const smallPen = document.querySelector('#small-pen')
+// bigPen.onclick = () => {
+//   ctx.lineWidth = 20
+// }
+// normalPen.onclick = () => {
+//   ctx.lineWidth = 10
+// }
+// smallPen.onclick = ()=>{
+//   ctx.lineWidth = 5
+// }
